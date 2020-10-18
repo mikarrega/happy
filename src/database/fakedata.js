b625d2ad893851e8ba96e318f0876e4a31183ea6
Michaela Dafne da Silva <michaela.silva@mindworks.com.br>
2020-10-18 12:59:44 -0300

    first commit

---
+++ src/database/fakedata.js
@@ -0,0 +1,33 @@
+module.exports = [
+    {
+        id: 1,
+        lat: "-20.2888299",
+        lng: "-40.3135136",
+        name: "Lar das meninas",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+            "http://127.0.0.1:5500/images/home.jpg",
+            "http://127.0.0.1:5500/images/home.jpg",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "1"
+    },
+
+
+    {
+        id: 2,
+        lat: "-20.2888299",
+        lng: "-40.3235136",
+        name: "Lar dos meninos",
+        description: "Presta assistência a crianças de 06 a 15 anos que se encontre em situação de risco e/ou vunerabilidade social.",
+        images: [
+            "http://127.0.0.1:5500/images/home.jpg",
+            "http://127.0.0.1:5500/images/home.jpg",
+        ],
+        instructions: "Venha como se sentir a vontade e traga muito amor e paciência para dar.",
+        opening_hours: "Horário de visitas das 18h até 8h",
+        open_on_weekends: "0"
+    },
+
+]
